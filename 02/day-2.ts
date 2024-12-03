e8d7f99e522243f1d9327630ceaa4a8dc3e7c94c
Clément Duployez <c.duployez@gmail.com>
2024-12-03 23:40:07 +0100

    Add day 2 - Part 2

--- 02/day-2.ts
+++ 02/day-2.ts
@@ -4,7 +4,14 @@ import {Report} from './report';
 class Day2 {
     static part1(inputFilePath: string): number {
         return new ReportInput(inputFilePath).parse()
-            .map(levels => new Report(levels))
+            .map(levels => new Report(levels, 0))
+            .filter(report => report.isSafe())
+            .length;
+    }
+
+    static part2(inputFilePath: string): number {
+        return new ReportInput(inputFilePath).parse()
+            .map(levels => new Report(levels, 1))
             .filter(report => report.isSafe())
             .length;
     }
@@ -12,5 +19,5 @@ class Day2 {
 
 console.log('Part 1 - Example: ', Day2.part1('example-input.txt')); // 2
 console.log('Part 1 - Input: ', Day2.part1('puzzle-input.txt')); // 463
-// console.log('Part 2 - Example: ', Day2.part2('example-input.txt'));
-// console.log('Part 2 - Input: ', Day2.part2('puzzle-input.txt'));
+console.log('Part 2 - Example: ', Day2.part2('example-input.txt')); // 4
+console.log('Part 2 - Input: ', Day2.part2('puzzle-input.txt')); // 514
--- 02/report.ts
+++ 02/report.ts
@@ -1,37 +1,53 @@
 export class Report {
-    constructor(private readonly levels: number[], private readonly minDelta: number = 1, private readonly maxDelta: number = 3) {
+    constructor(private readonly levels: number[],
+                private readonly nbRemovable: number,
+                private readonly minDelta: number = 1,
+                private readonly maxDelta: number = 3) {
 
     }
 
-    isSafe(): boolean {
-        return this.isIncreasingOrder() || this.isDecreasingOrder();
+    isSafe(levels: number[] = this.levels, nbRemoved: number = 0): boolean {
+        if (this.isIncreasingOrder(levels) || this.isDecreasingOrder(levels)) {
+            return true;
+        }
+        if (this.nbRemovable > nbRemoved) {
+            const levelCopies = this.toLevelCopies();
+            return levelCopies.some(levels => this.isSafe(levels, nbRemoved + 1));
+        }
+    }
+
+    toLevelCopies(): number[][] {
+        return this.levels.map((level, index) => {
+            const copy = [...this.levels];
+            copy.splice(index, 1);
+            return copy;
+        });
     }
 
     /**
      * Each level must increase by at least one and at most threee
      */
-    private isIncreasingOrder(): boolean {
-        return this.levels.every((level, index) => {
+    private isIncreasingOrder(levels: number[]): boolean {
+        return levels.every((level, index) => {
             if (index === 0) {
                 return true;
             }
             // Must increase by at least minDelta
-            return level - this.levels[index - 1] >= this.minDelta
+            return level - levels[index - 1] >= this.minDelta
                 // And at most maxDelta
-                && level - this.levels[index - 1] <= this.maxDelta;
+                && level - levels[index - 1] <= this.maxDelta;
         });
-
     }
 
-    private isDecreasingOrder(): boolean {
-        return this.levels.every((level, index) => {
+    private isDecreasingOrder(levels: number[]): boolean {
+        return levels.every((level, index) => {
             if (index === 0) {
                 return true;
             }
             // Must decrease by at least minDelta
-            return this.levels[index - 1] - level >= this.minDelta
+            return levels[index - 1] - level >= this.minDelta
                 // And at most maxDelta
-                && this.levels[index - 1] - level <= this.maxDelta;
+                && levels[index - 1] - level <= this.maxDelta;
         });
     }
 }
