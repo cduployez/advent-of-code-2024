4a98f40440265fdc153bf5e25d291e557ddedb70
Clément Duployez <c.duployez@gmail.com>
2024-12-03 23:02:24 +0100

    Add day 1 - Part 2

--- 01/distance-utils.ts
+++ 01/distance-utils.ts
@@ -5,7 +5,15 @@ export class DistanceUtils {
         const sortedLeftList = leftList.sort();
         const sortedRightList = rightList.sort();
         const pairs = sortedLeftList.map((leftValue, index) => new Pair(leftValue, sortedRightList[index]));
-        console.log(pairs.map(p => p.distance()));
         return pairs.reduce((acc, pair) => acc + pair.distance(), 0);
     };
+
+    static similarityScore(leftList: number[], rightList: number[]): number {
+        let score = 0;
+        leftList.forEach(leftValue => {
+            let nbRightOccurrences = rightList.filter(rightValue => rightValue === leftValue).length;
+            score += leftValue * nbRightOccurrences;
+        });
+        return score;
+    }
 }
--- 01/main.ts
+++ 01/main.ts
@@ -2,9 +2,18 @@ import {DistanceUtils} from './distance-utils';
 import {PuzzleInput} from './puzzle-input';
 
 class Main {
-    static main(inputFile: string): number {
-        return DistanceUtils.distanceSum(...new PuzzleInput(inputFile).parse());
+    static part1(inputFile: string): number {
+        const [leftList, rightList] = new PuzzleInput(inputFile).parse();
+        return DistanceUtils.distanceSum(leftList, rightList);
+    }
+
+    static part2(inputFile: string): number {
+        const [leftList, rightList] = new PuzzleInput(inputFile).parse();
+        return DistanceUtils.similarityScore(leftList, rightList);
     }
 }
 
-console.log(Main.main('puzzle-input.txt')); // 1530215
+console.log('Part 1 - Example: ', Main.part1('example-input.txt')); // 11
+console.log('Part 1 - Input: ', Main.part1('puzzle-input.txt')); // 1530215
+console.log('Part 2 - Example: ', Main.part2('example-input.txt')); // 31
+console.log('Part 2 - Input: ', Main.part2('puzzle-input.txt')); // 26800609
