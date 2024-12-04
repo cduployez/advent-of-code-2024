fe1d6ff48f2ff786d8ce39188d45d3e49f2da019
Clément Duployez <c.duployez@gmail.com>
2024-12-04 19:10:12 +0100

    Add Day 4 - Part 2

--- 04/cross-word.ts
+++ 04/cross-word.ts
@@ -1,92 +1,36 @@
 export class CrossWord {
 
-    static countWord(lines: string[], word: string): number {
-        const horizontal = this.countWordHorizontal(lines, word);
-        const horizontalReverse = this.countWordHorizontalReverse(lines, word);
-        const vertical = this.countWordVertical(lines, word);
-        const verticalReverse = this.countWordVerticalReverse(lines, word);
-        const diagonalLeftToRight = this.countWordDiagonalLeftToRight(lines, word);
-        const diagonalLeftToRightReverse = this.countWordDiagonalLeftToRightReverse(lines, word);
-        const diagonalRightToLeft = this.countWordDiagonalRightToLeft(lines, word);
-        const diagonalRightToLeftReverse = this.countWordDiagonalRightToLeftReverse(lines, word);
-        return horizontal + horizontalReverse + vertical + verticalReverse + diagonalLeftToRight + diagonalLeftToRightReverse + diagonalRightToLeft + diagonalRightToLeftReverse;
-    }
-
-    private static countWordHorizontal(lines: string[], word: string): number {
+    /**
+     * Count the number of times the gridChars appear in the lineChars. The '.' char means any character.
+     */
+    static countGrid(lineChars: string[][], gridChars: string[][]): number {
         let count = 0;
-        lines.forEach(l => {
-            let lineCount = l.split(word).length - 1;
-            count += lineCount;
-        });
-        return count;
-    }
-
-    private static countWordHorizontalReverse(lines: string[], word: string): number {
-        return this.countWordHorizontal(lines, CrossWord.reverse(word));
-    }
-
-    private static countWordVertical(lines: string[], word: string): number {
-        let count = 0;
-        for (let charIndex = 0; charIndex < lines[0].length; charIndex++) {
-            let line = '';
-            lines.forEach(l => {
-                line += l[charIndex];
-            });
-            let lineCount = line.split(word).length - 1;
-            count += lineCount;
+        for (let rowIndex = 0; rowIndex < lineChars.length; rowIndex++) {
+            for (let columnIndex = 0; columnIndex < lineChars[rowIndex].length; columnIndex++) {
+                if (lineChars[rowIndex][columnIndex] === gridChars[0][0]) {
+                    let found = true;
+                    for (let gridRowIndex = 0; gridRowIndex < gridChars.length; gridRowIndex++) {
+                        for (let gridColumnIndex = 0; gridColumnIndex < gridChars[gridRowIndex].length; gridColumnIndex++) {
+                            if (lineChars[rowIndex + gridRowIndex]?.[columnIndex + gridColumnIndex] !== gridChars[gridRowIndex][gridColumnIndex] && gridChars[gridRowIndex][gridColumnIndex] !== '.') {
+                                found = false;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            break;
+                        }
+                    }
+                    if (found) {
+                        count++;
+                    }
+                }
+            }
         }
         return count;
     }
 
-    private static countWordVerticalReverse(lines: string[], word: string): number {
-        return this.countWordVertical(lines, CrossWord.reverse(word));
-    }
-
-    private static countWordDiagonalLeftToRight(lines: string[], word: string): number {
-        // Top left to bottom right, upper half
-        const upperHalfDiagonalLines = [];
-        for (let rowIndex = 0; rowIndex < lines.length; rowIndex++) {
-            let line = '';
-            for (let columnIndex = 0; columnIndex < lines[rowIndex].length; columnIndex++) {
-                if (rowIndex + columnIndex < lines.length) {
-                    line += lines[rowIndex + columnIndex][columnIndex];
-                }
-            }
-            upperHalfDiagonalLines.push(line);
-        }
-        // Top left to bottom right, lower half
-        const lowerHalfDiagonalLines = [];
-        // Start from 1 to avoid the middle diagonal line
-        for (let rowIndex = 1; rowIndex < lines.length; rowIndex++) {
-            let line = '';
-            for (let columnIndex = 0; columnIndex < lines[rowIndex].length; columnIndex++) {
-                if (columnIndex + rowIndex < lines.length) {
-                    line += lines[columnIndex][columnIndex + rowIndex];
-                }
-            }
-            lowerHalfDiagonalLines.push(line);
-        }
-        return this.countWordHorizontal(upperHalfDiagonalLines, word) + this.countWordHorizontal(lowerHalfDiagonalLines, word);
-    }
-
-    private static countWordDiagonalLeftToRightReverse(lines: string[], word: string): number {
-        return this.countWordDiagonalLeftToRight(lines, CrossWord.reverse(word));
-    }
-
-    private static countWordDiagonalRightToLeft(lines: string[], word: string): number {
-        return this.countWordDiagonalLeftToRight(CrossWord.reverseLines(lines), word);
-    }
-
-    private static countWordDiagonalRightToLeftReverse(lines: string[], word: string): number {
-        return this.countWordDiagonalLeftToRight(CrossWord.reverseLines(lines), CrossWord.reverse(word));
-    }
-
-    private static reverse(text: string): string {
-        return text.split('').reverse().join('');
-    }
-
-    private static reverseLines(lines: string[]): string[] {
-        return lines.map(l => CrossWord.reverse(l));
+    static reverseLines(lines: string[][]): string[][] {
+        return lines.map(l => l.reverse());
     }
 
 
--- 04/day-4.ts
+++ 04/day-4.ts
@@ -3,16 +3,57 @@ import {CrossWord} from './cross-word';
 
 class Day4 {
     static part1(inputFilePath: string): number {
-        const lines: string[] = new CrossWordInput(inputFilePath).parse();
-        return CrossWord.countWord(lines, 'XMAS');
+        const lines: string[][] = new CrossWordInput(inputFilePath).parse();
+        const horizontalGrid: string[][] = [['X', 'M', 'A', 'S']];
+        const horizontalCount = CrossWord.countGrid(lines, horizontalGrid);
+        const horizontalReverseGrid: string[][] = [['S', 'A', 'M', 'X']];
+        const horizontalReverseCount = CrossWord.countGrid(lines, horizontalReverseGrid);
+        const verticalGrid: string[][] = [['X'], ['M'], ['A'], ['S']];
+        const verticalCount = CrossWord.countGrid(lines, verticalGrid);
+        const verticalReverseGrid: string[][] = [['S'], ['A'], ['M'], ['X']];
+        const verticalReverseCount = CrossWord.countGrid(lines, verticalReverseGrid);
+        const diagonalTopLeftToBottomRightGrid = `X...
+.M..
+..A.
+...S`.split('\n').map(l => l.split(''));
+        const diagonalTopLeftToBottomRightCount = CrossWord.countGrid(lines, diagonalTopLeftToBottomRightGrid);
+        const diagonalTopLeftToBottomRightReverseGrid = `S...
+.A..
+..M.
+...X`.split('\n').map(l => l.split(''));
+        const diagonalTopLeftToBottomRightReverseCount = CrossWord.countGrid(lines, diagonalTopLeftToBottomRightReverseGrid);
+        const diagonalTopRightToBottomLeftCount = CrossWord.countGrid(CrossWord.reverseLines(lines), diagonalTopLeftToBottomRightGrid);
+        const diagonalTopRightToBottomLeftReverseCount = CrossWord.countGrid(CrossWord.reverseLines(lines), diagonalTopLeftToBottomRightReverseGrid);
+        return horizontalCount + verticalCount + horizontalReverseCount + verticalReverseCount + diagonalTopLeftToBottomRightCount + diagonalTopLeftToBottomRightReverseCount + diagonalTopRightToBottomLeftCount + diagonalTopRightToBottomLeftReverseCount;
     }
 
     static part2(inputFilePath: string): number {
-        return 0;
+        const lines: string[][] = new CrossWordInput(inputFilePath).parse();
+        const grid1: string[][] = `M.S
+.A.
+M.S`.split('\n').map(l => l.split(''));
+        const grid1Count = CrossWord.countGrid(lines, grid1);
+
+        const grid2: string[][] = `S.M
+.A.
+S.M`.split('\n').map(l => l.split(''));
+        const grid2Count = CrossWord.countGrid(lines, grid2);
+
+        const grid3: string[][] = `S.S
+.A.
+M.M`.split('\n').map(l => l.split(''));
+        const grid3Count = CrossWord.countGrid(lines, grid3);
+
+        const grid4: string[][] = `M.M
+.A.
+S.S`.split('\n').map(l => l.split(''));
+        const grid4Count = CrossWord.countGrid(lines, grid4);
+
+        return grid1Count + grid2Count + grid3Count + grid4Count;
     }
 }
 
 console.log('Part 1 - Example: ', Day4.part1('example-input.txt')); // 18
 console.log('Part 1 - Input: ', Day4.part1('puzzle-input.txt')); // 2654
-// console.log('Part 2 - Example: ', Day4.part2('example-input.txt')); //
-// console.log('Part 2 - Input: ', Day4.part2('puzzle-input.txt')); //
+console.log('Part 2 - Example: ', Day4.part2('example-input.txt')); // 9
+console.log('Part 2 - Input: ', Day4.part2('puzzle-input.txt')); // 1990
