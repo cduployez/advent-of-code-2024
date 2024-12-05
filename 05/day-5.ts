8364c3b790c934e8e679231347876fce91017b0b
Clément Duployez <c.duployez@gmail.com>
2024-12-05 23:49:48 +0100

    Add Day 5 - Part 2

--- 05/day-5.ts
+++ 05/day-5.ts
@@ -9,11 +9,12 @@ class Day5 {
     }
 
     static part2(inputFilePath: string): number {
-        return 0;
+        const input: InputResult = new PageInput(inputFilePath).parse();
+        return UpdatePageNumberUtils.addUnverifiedFixedMiddlePageNumbers(input.updatePageNumbers, input.pageOrderingRuleMap);
     }
 }
 
 console.log('Part 1 - Example: ', Day5.part1('example-input.txt')); // 143
 console.log('Part 1 - Input: ', Day5.part1('puzzle-input.txt')); // 4905
-// console.log('Part 2 - Example: ', Day5.part2('example-input.txt')); //
-// console.log('Part 2 - Input: ', Day5.part2('puzzle-input.txt')); //
+console.log('Part 2 - Example: ', Day5.part2('example-input.txt')); // 123
+console.log('Part 2 - Input: ', Day5.part2('puzzle-input.txt')); // 6204
--- 05/update-page-number-utils.ts
+++ 05/update-page-number-utils.ts
@@ -2,13 +2,17 @@ import {PageOrderingRule} from './page-ordering-rule';
 
 export class UpdatePageNumberUtils {
 
+    static containsNextPageNumber(pageNumber: number, nextPageNumber: number, pageOrderingRuleMap: Record<number, PageOrderingRule>): boolean {
+        return pageOrderingRuleMap[pageNumber].nextPageOrderingRules.some(rule => rule.pageNumber === nextPageNumber);
+    }
+
     static verify(updatePageNumbers: number[], pageOrderingRuleMap: Record<number, PageOrderingRule>): boolean {
         let pageNumber: number;
         let nextPageNumber: number;
         for (let i = 1; i < updatePageNumbers.length; i++) {
             pageNumber = updatePageNumbers[i - 1];
             nextPageNumber = updatePageNumbers[i];
-            if (!pageOrderingRuleMap[pageNumber].nextPageOrderingRules.some(rule => rule.pageNumber === nextPageNumber)) {
+            if (!UpdatePageNumberUtils.containsNextPageNumber(pageNumber, nextPageNumber, pageOrderingRuleMap)) {
                 return false;
             }
         }
@@ -29,4 +33,38 @@ export class UpdatePageNumberUtils {
         return middleNumbers.reduce((acc, num) => acc + num, 0);
     }
 
+    static addUnverifiedFixedMiddlePageNumbers(updatePageNumbers: number[][], pageOrderingRuleMap: Record<number, PageOrderingRule>): number {
+        const unverifiedUpdatePageNumbers: number[][] = updatePageNumbers.filter(update => !UpdatePageNumberUtils.verify(update, pageOrderingRuleMap));
+        const middleNumbers: number[] = [];
+        unverifiedUpdatePageNumbers.forEach(update => {
+            const fixedUnverifiedUpdatePageNumbers: number[] = UpdatePageNumberUtils.fixUnverifiedUpdatePageNumbers(update, pageOrderingRuleMap);
+            middleNumbers.push(fixedUnverifiedUpdatePageNumbers[(fixedUnverifiedUpdatePageNumbers.length - 1) / 2]);
+        });
+        console.log('Middle numbers:', middleNumbers);
+        // Addition of middle numbers
+        return middleNumbers.reduce((acc, num) => acc + num, 0);
+    }
+
+    private static fixUnverifiedUpdatePageNumbers(update: number[], pageOrderingRuleMap: Record<number, PageOrderingRule>, level: number = 0): number[] {
+        const remainingNumbers = [...update];
+        let fixedUpdatePageNumbers: number[] = [remainingNumbers.shift()];
+        while (remainingNumbers.length > 0) {
+            for (let i = 0; i < remainingNumbers.length; i++) {
+                if (UpdatePageNumberUtils.containsNextPageNumber(fixedUpdatePageNumbers[fixedUpdatePageNumbers.length - 1], remainingNumbers[i], pageOrderingRuleMap)) {
+                    fixedUpdatePageNumbers.push(remainingNumbers.splice(i, 1)[0]);
+                    break;
+                } else if (UpdatePageNumberUtils.containsNextPageNumber(remainingNumbers[i], fixedUpdatePageNumbers[fixedUpdatePageNumbers.length - 1], pageOrderingRuleMap)) {
+                    fixedUpdatePageNumbers.unshift(remainingNumbers.splice(i, 1)[0]);
+                    break;
+                }
+            }
+        }
+        while (!UpdatePageNumberUtils.verify(fixedUpdatePageNumbers, pageOrderingRuleMap)) {
+            fixedUpdatePageNumbers = UpdatePageNumberUtils.fixUnverifiedUpdatePageNumbers(fixedUpdatePageNumbers, pageOrderingRuleMap, level + 1);
+        }
+        if (level === 0) {
+            console.log('Fixed update:', fixedUpdatePageNumbers, this.verify(fixedUpdatePageNumbers, pageOrderingRuleMap));
+        }
+        return fixedUpdatePageNumbers;
+    }
 }
