471e2748e9aa1843465d69fdb758a64a0517f390
Clément Duployez <c.duployez@gmail.com>
2024-12-11 17:22:22 +0100

    Add Day 9 - Part 2

--- 09/day-9.ts
+++ 09/day-9.ts
@@ -1,22 +1,30 @@
 import {Input} from '../input';
 
 class Block {
-    id: number | null;
+    id: number | null | undefined;
     length: number;
 
 
-    constructor(id: number | null, length: number) {
+    constructor(id: number | null | undefined, length: number) {
         this.id = id;
         this.length = length;
     }
 
-    toIndividualBlocks(): number[] | null[] {
-        return new Array<number>(this.length).fill(this.id);
+    toIndividualBlocks(): (number | null)[] {
+        return new Array<number | null>(this.length).fill(this.id !== undefined ? this.id : null);
+    }
+
+    copy(): Block {
+        return new Block(this.id, this.length);
     }
 }
 
 class DiskMap {
-    blocks: Block[] = [];
+    blocks: Block[];
+
+    constructor(blocks: Block[] = []) {
+        this.blocks = blocks;
+    }
 
     displayDenseFormat(): void {
         console.log(this.blocks.map(block => block.length).join(''));
@@ -30,14 +38,14 @@ class DiskMap {
         return this.blocks.map(b => b.toIndividualBlocks()).flat();
     }
 
-    compact(): DiskMap {
+    compactFragmented(): DiskMap {
         let individualBlocks = this.toIndividualBlocks();
         const nbEmpty = individualBlocks.filter(id => id === null).length;
-        const compactedBlocks: (number | null)[] = [...individualBlocks];
+        const compactedBlocks: (number | null | undefined)[] = [...individualBlocks];
         let i = 0;
         while (i < compactedBlocks.length) {
             const currentBlock = compactedBlocks[i];
-            if (currentBlock === null) {
+            if (currentBlock === null || currentBlock === undefined) {
                 let poppedBlock = compactedBlocks.pop();
                 while (poppedBlock === null) {
                     poppedBlock = compactedBlocks.pop();
@@ -52,6 +60,31 @@ class DiskMap {
         return DiskMap.fromIndividualBlocks(compactedBlocks);
     }
 
+    compactBlocks(): DiskMap {
+        const blocks = [...this.blocks.map(b => b.copy())];
+        for(let i = 0; i < blocks.length; i++) {
+            let emptyBlock = blocks[i];
+            if (emptyBlock.id === null) {
+                for(let j = blocks.length - 1; j > i; j--) {
+                    let nextBlock = blocks[j];
+                    if (nextBlock.id !== null && nextBlock.length <= emptyBlock.length) {
+                        let remainingLength = emptyBlock.length - nextBlock.length;
+                        emptyBlock.length = nextBlock.length;
+                        emptyBlock.id = nextBlock.id;
+                        nextBlock.id = null;
+                        if (remainingLength > 0) {
+                            // Insert a new block after i
+                            blocks.splice(i + 1, 0, new Block(null, remainingLength));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new DiskMap(blocks);
+    }
+
     toFilesystemChecksum(): number {
         let sum = 0;
         this.toIndividualBlocks().forEach((value, index) => {
@@ -60,25 +93,27 @@ class DiskMap {
         return sum;
     }
 
-    static fromDenseFormat(line: (number | null)[]): DiskMap {
+    static fromDenseFormat(line: (number | null | undefined)[]): DiskMap {
         const diskMap = new DiskMap();
         let id = 0;
         let isBlock = true;
         diskMap.blocks = line.map((value, index) => {
-            const block = new Block(isBlock ? id++ : null, value);
+            const block = new Block(isBlock ? id++ : null, value || 0);
             isBlock = !isBlock;
             return block;
         });
         return diskMap;
     }
 
-    static fromIndividualBlocks(line: (number | null)[]): DiskMap {
+    static fromIndividualBlocks(line: (number | null | undefined)[]): DiskMap {
         const diskMap = new DiskMap();
         let currentValue: number | null | undefined = undefined;
         let currentBlock: Block | null = null;
         line.forEach(value => {
             if (currentValue === value) {
-                currentBlock.length++;
+                if (currentBlock) {
+                    currentBlock.length++;
+                }
             } else {
                 currentValue = value;
                 currentBlock = new Block(value, 1);
@@ -102,7 +137,7 @@ class Day9 {
         const diskMap = new DiskMapInput(inputFilePath).parse();
         diskMap.displayDenseFormat();
         diskMap.displayIndividualBlocks();
-        const compactedDiskMap = diskMap.compact();
+        const compactedDiskMap = diskMap.compactFragmented();
         console.log('\nCompacted');
         compactedDiskMap.displayDenseFormat();
         compactedDiskMap.displayIndividualBlocks();
@@ -111,16 +146,24 @@ class Day9 {
     }
 
     static part2(inputFilePath: string): number {
-        return 0;
+        const diskMap = new DiskMapInput(inputFilePath).parse();
+        diskMap.displayDenseFormat();
+        diskMap.displayIndividualBlocks();
+        const compactedDiskMap = diskMap.compactBlocks();
+        console.log('\nCompacted');
+        compactedDiskMap.displayDenseFormat();
+        compactedDiskMap.displayIndividualBlocks();
+        console.log('\nChecksum');
+        return compactedDiskMap.toFilesystemChecksum();
     }
 }
 
 const startTime = performance.now();
-// console.log('Part 1 - Tutorial: ', Day9.part1('tutorial-input.txt')); // 60
-// console.log('Part 1 - Example: ', Day9.part1('example-input.txt')); // 1928
+console.log('Part 1 - Tutorial: ', Day9.part1('tutorial-input.txt')); // 60
+console.log('Part 1 - Example: ', Day9.part1('example-input.txt')); // 1928
 console.log('Part 1 - Input: ', Day9.part1('puzzle-input.txt')); // 6463499258318
-// console.log('Part 2 - Example: ', Day9.part2('example-input.txt')); //
-// console.log('Part 2 - Input: ', Day9.part2('puzzle-input.txt')); //
+console.log('Part 2 - Example: ', Day9.part2('example-input.txt')); // 2858
+console.log('Part 2 - Input: ', Day9.part2('puzzle-input.txt')); // 6493634986625
 const endTime = performance.now();
 console.log(`Call to method took ${endTime - startTime} milliseconds`);
 
