8b2bb31a6f1cc812ccd5060171aaa9ed795acbf2
Clément Duployez <c.duployez@gmail.com>
2024-12-14 14:55:07 +0100

    Add Day 14 - Part 2

--- 14/day-14.ts
+++ 14/day-14.ts
@@ -12,7 +12,12 @@ class Room {
         this.robots = robots;
     }
 
-    displayRoom(): void {
+    displayRoom(nbSeconds: number, onlyIfRobotsClose: boolean): void {
+        if (onlyIfRobotsClose) {
+            this.displayRoomIfManyRobotsClose(nbSeconds);
+            return;
+        }
+        console.log(`After ${nbSeconds} seconds:`);
         for (let y = 0; y < this.height; y++) {
             let line = '';
             for (let x = 0; x < this.width; x++) {
@@ -23,15 +28,38 @@ class Room {
         }
     }
 
+    /**
+     * We want a square of 5x5 with at least 1 robot in each cell
+     */
+    displayRoomIfManyRobotsClose(nbSeconds: number): void {
+        const lines: string[] = [];
+        for (let y = 0; y < this.height; y++) {
+            let line = '';
+            for (let x = 0; x < this.width; x++) {
+                const nbRobots = this.robots.filter(robot => robot.position.x === x && robot.position.y === y).length;
+                line += nbRobots === 0 ? '.' : nbRobots;
+            }
+            lines.push(line);
+        }
+
+        const filteredLines = lines.filter((line, index) => {
+            return (line.match(/[^.]{5}/g)?.length || 0) > 0;
+        });
+
+        if (filteredLines.length > 0) {
+            console.log(`After ${nbSeconds} seconds:`);
+            lines.forEach(line => console.log(line));
+        }
+    }
+
     moveRobots(): void {
         this.robots.forEach(robot => this.move(robot));
     }
 
-    moveNbSeconds(nbSeconds: number): void {
+    moveNbSeconds(nbSeconds: number, displayOnlyIfRobotsClose: boolean): void {
         for (let i = 0; i < nbSeconds; i++) {
-            console.log(`After ${i + 1} seconds:`);
             this.moveRobots();
-            this.displayRoom();
+            this.displayRoom(i, displayOnlyIfRobotsClose);
         }
     }
 
@@ -110,22 +138,26 @@ class Day14 {
     static part1(inputFilePath: string, width: number, height: number, nbSeconds: number): number {
         const robots = new RobotsInput(inputFilePath).parse();
         const room = new Room(width, height, robots);
-        room.displayRoom();
-        room.moveNbSeconds(nbSeconds);
+        room.displayRoom(0, false);
+        room.moveNbSeconds(nbSeconds, false);
         return room.multiplyNbRobotsOfQuadrants();
     }
 
-    static part2(inputFilePath: string): number {
-        return 0;
+    static part2(inputFilePath: string, width: number, height: number, nbSeconds: number): number {
+        const robots = new RobotsInput(inputFilePath).parse();
+        const room = new Room(width, height, robots);
+        room.displayRoom(0, false);
+        console.log('Moving...');
+        room.moveNbSeconds(nbSeconds, true);
+        return room.multiplyNbRobotsOfQuadrants();
     }
 }
 
 const startTime = performance.now();
 // console.log('Part 1 - Example: ', Day14.part1('simple-input.txt', 11, 7, 10));
 // console.log('Part 1 - Example: ', Day14.part1('example-input.txt', 11, 7, 100)); // 12
-console.log('Part 1 - Input: ', Day14.part1('puzzle-input.txt', 101, 103, 100)); //
-// console.log('Part 2 - Input: ', Day14.part2('exqmple-input.txt')); //
-// console.log('Part 2 - Input: ', Day14.part2('puzzle-input.txt')); //
+// console.log('Part 1 - Input: ', Day14.part1('puzzle-input.txt', 101, 103, 100)); // 230900224
+console.log('Part 2 - Input: ', Day14.part2('puzzle-input.txt', 101, 103, 6531)); // 6532
 const endTime = performance.now();
 console.log(`Call to method took ${endTime - startTime} milliseconds`);
 
